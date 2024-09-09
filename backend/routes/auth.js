220cac7341f1bc0badb0c3dd1fcc4dc333595d0f
Prashant Kumar Jha <prashantkumarjha.cse2022@nsec.ac.in>
2024-09-09 22:27:47 +0530

    45c

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,18 @@
 const express= require('express');
-const router=express.Router()
+const User=require('../models/User');
+const router=express.Router();
 
-router.get('/',(req,res) =>{
-    obj={
-        a:'thios',
-        number:34
-    }
-    res.json(obj)
+
+//create a user using : POST "/api/auth". Doesn,t require Auth
+
+router.post('/',(req,res) =>{
+    
+    console.log(req.body);
+    const user=User(req.body);
+    user.save()
+    res.send(req.body);
+
 })
 
-module.exports = router
+
+module.exports = router;
