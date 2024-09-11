75fd292b8fa1dad22cb34d0059be5291bfee5751
Prashant Kumar Jha <prashantkumarjha.cse2022@nsec.ac.in>
2024-09-11 12:29:16 +0530

    49

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,10 +2,12 @@ const express= require('express');
 const User=require('../models/User');
 const router=express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 
 //create a user using : POST "/api/auth/createuser".No login required
-
+const JWT_SECRET="prashantkhbvhg";
 router.post('/createuser',[
     body('name', 'Enter a valid Name').isLength({ min :3 }),
     body('email','Enter a valid e-mail' ).isEmail(),
@@ -22,19 +24,28 @@ router.post('/createuser',[
     if(user){
         return res.status(400).json({error: "Sorry a user with this email already exists"})
     }
+    const salt= await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt);
+    // Create a new user
     user= await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPass,
         email:req.body.email
       })
-    res.json(user);
+      const data= {
+    user:{
+        id: user.id
     }
-    catch{
+      }
+    const authToken=jwt.sign(data, JWT_SECRET);
+    
+    res.json(authToken);
+    }
+    catch(error){
         console.error(error.message);
         res.status(500).send("Some errors occuurred");
     }
 
 })
-
 
 module.exports = router;
