5afd6464b98f10f4ea86744f30f2de0327bde463
Prashant Kumar Jha <prashantkumarjha.cse2022@nsec.ac.in>
2024-09-16 15:26:56 +0530

    50

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -43,9 +43,45 @@ router.post('/createuser',[
     }
     catch(error){
         console.error(error.message);
-        res.status(500).send("Some errors occuurred");
+        res.status(500).send("Internal Server errors occuurred");
     }
 
 })
+//Authenticate a user using : POST "/api/auth/login".No login required
 
+router.post('/login',[
+    body('email','Enter a valid e-mail' ).isEmail(),
+    body('password', 'password cannot be blank').exists(),
+],async(req,res) =>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array() });
+    }
+    const {email, password}= req.body;
+    try{
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error : "Please try to login with correct credentials"});
+        }
+
+        const passwordCompare =await  bcrypt.compare(password, user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error : "Please try to login with correct credentials"});
+        }
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken=jwt.sign(payload, JWT_SECRET);
+    
+    res.json(authToken);
+    }
+    catch(error){
+        console.error(error.message);
+        res.status(500).send("Internal Server errors occuurred");
+    }
+    
+
+})
 module.exports = router;
